e6677015cdd746cad323ab7ff96fd9c70edd08df
renwid <widjajarenzo1@gmail.com>
2019-03-25 20:27:13 +0000

    Added Bootstrap Files

---
+++ register.php
@@ -0,0 +1,160 @@
+<?php
+//register.php
+/**
+ * Start the session.
+ */
+session_start();
+/**
+ * Include ircmaxell's password_compat library.
+ */
+require 'lib/password.php';
+/**
+ * Include our MySQL connection.
+ */
+require 'login_connect.php';
+//If the POST var "register" exists (our submit button), then we can
+//assume that the user has submitted the registration form.
+if(isset($_POST['register'])){
+
+    //Retrieve the field values from our registration form.
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $pass = !empty($_POST['password']) ? trim($_POST['password']) : null;
+
+    //TO ADD: Error checking (username characters, password length, etc).
+    //Basically, you will need to add your own error checking BEFORE
+    //the prepared statement is built and executed.
+
+    //Now, we need to check if the supplied username already exists.
+
+    //Construct the SQL statement and prepare it.
+    $sql = "SELECT COUNT(username) AS num FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($sql);
+
+    //Bind the provided username to our prepared statement.
+    $stmt->bindValue(':username', $username);
+
+    //Execute.
+    $stmt->execute();
+
+    //Fetch the row.
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //If the provided username already exists - display error.
+    //TO ADD - Your own method of handling this error. For example purposes,
+    //I'm just going to kill the script completely, as error handling is outside
+    //the scope of this tutorial.
+    if($row['num'] > 0){
+        die('That username already exists!');
+    }
+
+    //Hash the password as we do NOT want to store our passwords in plain text.
+    $passwordHash = password_hash($pass, PASSWORD_BCRYPT, array("cost" => 12));
+
+    //Prepare our INSERT statement.
+    //Remember: We are inserting a new row into our users table.
+    $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+    $stmt = $pdo->prepare($sql);
+
+    //Bind our variables.
+    $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':password', $passwordHash);
+    //Execute the statement and insert the new account.
+    $result = $stmt->execute();
+
+    //If the signup process is successful.
+    if($result){
+        //What you do here is up to you!
+        echo 'Thank you for registering with our website.';
+    }
+
+}
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin 2 - Register</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <div class="card o-hidden border-0 shadow-lg my-5">
+      <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+        <div class="row">
+          <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+          <div class="col-lg-7">
+            <div class="p-5">
+              <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+              </div>
+              <form class="user" action="register1.php" method="post">
+
+
+                  <div class="col-sm-6 mb-3 mb-sm-11">
+                    <input name="username" type="text" class="form-control form-control-user" id="username" placeholder="User Name">
+                  </div>
+
+                  <div class="col-sm-6 mb-3 mb-sm-20">
+                    <input name="password" type="password" class="form-control form-control-user" id="password" placeholder="Password">
+                  </div>
+
+
+
+                  <button type="submit" name="register" value="Register" class="btn btn-primary btn-user btn-block">Register</button>
+
+
+                <hr>
+                <a href="index.html" class="btn btn-google btn-user btn-block">
+                  <i class="fab fa-google fa-fw"></i> Register with Google
+                </a>
+
+                <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                  <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                </a>
+
+              </form>
+              <hr>
+              <div class="text-center">
+                <a class="small" href="forgot-password.html">Forgot Password?</a>
+              </div>
+              <div class="text-center">
+                <a class="small" href="login.html">Already have an account? Login!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
