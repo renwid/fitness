a8380b9b0f57a6c35d0078563b4fcec26c63315e
D00211852 <D00211852@student.dkit.ie>
2019-03-29 16:34:07 +0000

    database

---
+++ Model/database.php
@@ -0,0 +1,14 @@
+<?php
+
+    $dsn = 'mysql:host=localhost;dbname=fitness';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        //include('../Error/database_error.php');
+        exit();
+    }
+?>
