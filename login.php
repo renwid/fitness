b7b7cb1d35da357a1b1a79e60ba8a961783fc726
renwid <widjajarenzo1@gmail.com>
2019-04-17 20:59:06 +0100

    User register/login page

---
+++ login.php
@@ -0,0 +1,191 @@
+<?php
+//register.php
+/**
+ * Start the session.
+ */
+session_start();
+/**
+ * Include ircmaxell's password_compat library.
+ */
+require 'lib/password.php';
+/**
+ * Include our MySQL connection.
+ */
+require 'login_connect.php';
+//If the POST var "register" exists (our submit button), then we can
+//assume that the user has submitted the registration form.
+if(isset($_POST['register'])){
+
+    //Retrieve the field values from our registration form.
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $pass = !empty($_POST['password']) ? trim($_POST['password']) : null;
+
+    //TO ADD: Error checking (username characters, password length, etc).
+    //Basically, you will need to add your own error checking BEFORE
+    //the prepared statement is built and executed.
+
+    //Now, we need to check if the supplied username already exists.
+
+    //Construct the SQL statement and prepare it.
+    $sql = "SELECT COUNT(username) AS num FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($sql);
+
+    //Bind the provided username to our prepared statement.
+    $stmt->bindValue(':username', $username);
+
+    //Execute.
+    $stmt->execute();
+
+    //Fetch the row.
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //If the provided username already exists - display error.
+    //TO ADD - Your own method of handling this error. For example purposes,
+    //I'm just going to kill the script completely, as error handling is outside
+    //the scope of this tutorial.
+    if($row['num'] > 0){
+        die('That username already exists!');
+    }
+
+    //Hash the password as we do NOT want to store our passwords in plain text.
+    $passwordHash = password_hash($pass, PASSWORD_BCRYPT, array("cost" => 12));
+
+    //Prepare our INSERT statement.
+    //Remember: We are inserting a new row into our users table.
+    $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+    $stmt = $pdo->prepare($sql);
+
+    //Bind our variables.
+    $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':password', $passwordHash);
+    //Execute the statement and insert the new account.
+    $result = $stmt->execute();
+
+    //If the signup process is successful.
+    if($result){
+        //What you do here is up to you!
+        echo 'Thank you for registering with our website.';
+    }
+
+}
+elseif(isset($_POST['login'])){
+
+    //Retrieve the field values from our login form.
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+
+    //Retrieve the user account information for the given username.
+    $sql = "SELECT id, username, password FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($sql);
+
+    //Bind value.
+    $stmt->bindValue(':username', $username);
+
+    //Execute.
+    $stmt->execute();
+
+    //Fetch row.
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //If $row is FALSE.
+    if($user === false){
+        //Could not find a user with that username!
+        //PS: You might want to handle this error in a more user-friendly manner!
+        die('Incorrect username / password combination!');
+    } else{
+        //User account found. Check to see if the given password matches the
+        //password hash that we stored in our users table.
+
+        //Compare the passwords.
+        $validPassword = password_verify($passwordAttempt, $user['password']);
+
+        //If $validPassword is TRUE, the login has been successful.
+        if($validPassword){
+
+            //Provide the user with a login session.
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['logged_in'] = time();
+
+            //Redirect to our protected page, which we called home.php
+            header('Location: add_profile.php');
+            exit;
+
+        } else{
+            //$validPassword was FALSE. Passwords do not match.
+            die('Incorrect username / password combination!');
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  </head>
+  <body>
+<div class="container" id="container">
+
+  <!-- Sign Up -->
+	<div class="form-container sign-up-container">
+		<form action="test.php" class="user" method="post">
+			<h1>Create Account</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your email for registration</span>
+			<input type="text" name="username" id="username" placeholder="Username" />
+			<input type="password" name="password" id="password" placeholder="Password" />
+			<button type="submit" name="register" value="Register">Sign Up</button>
+		</form>
+	</div>
+
+
+
+  <!-- Sign In -->
+	<div class="form-container sign-in-container">
+		<form class="user" action="test.php" method="post">
+			<h1>Sign in</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your account</span>
+      <input name="username" type="text" id="username" placeholder="Username" />
+      <input name="password" type="password" id="password" placeholder="Password">
+			<a href="#">Forgot your password?</a>
+			<button type="submit" name="login" value="Login">Sign In</button>
+		</form>
+	</div>
+
+
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+				<button class="ghost" id="signUp">Sign Up</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer>
+
+</footer>
+<script src="js/login.js"></script>
+
+  </body>
+</html>
